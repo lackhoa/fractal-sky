6a37f48f4bf1e13e3558d55dea28b1780690f6bf
lackhoa <vodangkhoa873@gmail.com>
2020-06-12 19:04:28 +0300

    Transitioned to hook

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 'use strict';
 let e = React.createElement;
-let i = Immutable;
+let { useState, useCallback, useMemo } = React;
 let log = console.log;
 
 // TODO: Prepend "Constants."
 let SVG_NS = "http://www.w3.org/2000/svg";
-
-const START_OF_DIAGRAM_TAG = "<diagram>";
-const END_OF_DIAGRAM_TAG = "</diagram>";
+let [W, H] = [801, 481];
 
 // Must be lowercase "shapename" - "shapeName", as set in the toolbox controller, the DOM adds elements as lowercase!
 // https://stackoverflow.com/a/6386486/2276361
@@ -80,9 +78,9 @@ function uuidv4() {
 }
 
 function translate(model, tx, ty) {
-    let [a,b,c,d,e,f] = model.get("transform") || [1,0,0,1,0,0];
+    let [a,b,c,d,e,f] = model.transform || [1,0,0,1,0,0];
     // Note that translation is scaled along with the transformation matrix
-    return model.set("transform", [a,b,c,d, e+tx, f+ty]);
+    return {...model, transform: [a,b,c,d, e+tx, f+ty]};
 }
 
 function cssMatrix(matrix) {  // `matrix` is a 6-array
@@ -90,13 +88,13 @@ function cssMatrix(matrix) {  // `matrix` is a 6-array
 }
 
 function shapeToCpn(model) {
-    // The model is just the tag + the props of the component, but with readable transform, and also immutable
-    let m = model.toJS();
-    let xform = m.transform;
+    // The model is just the tag + the props of the component, but with readable transform
+    let m = {...model};  // shallow copying
     // convert transform to CSS
+    let xform = model.transform;
     if (xform) {m.transform = cssMatrix(xform);}
-    let type = m.type; delete m.type;
-    return e(type, m);
+    delete m.type;
+    return e(model.type, m);
 }
 
 let smallGrid = e("pattern",
@@ -113,119 +111,110 @@ let largeGrid = e("pattern", {id:"largeGrid",
                   e("path", {id:"largeGridPath", d:"M 80 0 H 0 V 80",
                              fill:"none", stroke:"#AAAAAA", strokeWidth:2}));
 
-let commonShape = i.fromJS({fill:"transparent", stroke:"black",
-                            style: {cursor:"move"},
-                            vectorEffect: "non-scaling-stroke"});
-let rect = commonShape.merge({type:"rect", width:1, height:1,
-                              transform: [100, 0, 0, 100, 0, 0]});
-let circle = commonShape.merge({type:"circle", cx:0.5, cy:0.5, r:0.5,
-                                transform: [100, 0, 0, 100, 0, 0]});
+let commonShape = {fill:"transparent", stroke:"black",
+                   style: {cursor:"move"},
+                   vectorEffect: "non-scaling-stroke"};
+let rectModel = {...commonShape,
+                 type:"rect", width:1, height:1,
+                 transform: [100, 0, 0, 100, 0, 0]};
+let circleModel = {...commonShape,
+                   type:"circle", cx:0.5, cy:0.5, r:0.5,
+                   transform: [100, 0, 0, 100, 0, 0]};
 
 // Translation is applied to both the surface and the shapes
 // Shapes are under 2 translations: by the view and by user's arrangement
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        // `state` (rather `state.data`) must be immutable, we cannot afford it to change EVER (by React)!
-        this.state = {data: i.fromJS({shapes: {},  // Maps from ids to shape models
-                                      xform: [1,0,0,1,0,0],  // viewport transform
-                                      mouseDown: null,  // stores mouse down position
-                                      focused: null,  // Focused shape id
-                                     })};
-        this.rect = this.bindControl(rect);
-        this.circle = this.bindControl(circle);
-    }
+function App () {
+    let [state, setState] = useState({
+        shapes: {},  // Map from id to shapes model
+        xform: [1,0,0,1,0,0],  // Universal transformation
+        mouseDown: null,  // Previous mouse down position
+        focused: null,  // The focused shape id
+    });
 
-    bindControl(model) {
-        // Let the app control everything
-        return model.merge({onMouseDown: (evt) => this.onMouseDown(evt),
-                            onMouseMove: (evt) => this.onMouseMove(evt),
-                            onMouseUp: (evt) => this.onMouseUp(evt)});
-    }
-
-    // My replacement of "setState"
-    imSetState(data) {this.setState({data: data})}
-
-
-    onMouseDown(evt) {
-        let data = this.state.data;
+    // Using the updater callback, no need for depedency
+    let onMouseDown = useCallback((evt) => {
+        let id = evt.target.id;
         let pos = [evt.clientX, evt.clientY];
-        this.imSetState(data.merge({mouseDown: pos,
-                                    focused: evt.target.id}));
-    }
+        setState((state) => {
+            return {...state, mouseDown: pos, focused: id};
+        })
+    }, [])
 
-    onMouseMove(evt) {
-        let data = this.state.data;
-        let mouseDown = data.get("mouseDown");
-        // If dragging, move the focused shape
-        if (mouseDown) {
-            let [x0,y0] = mouseDown;
-            let [x,y] = [evt.clientX, evt.clientY];
-            let data1 = data.set("mouseDown", [x,y]);  // Update the mouse for future dragging
-            let focused = data.get("focused");
-            if (focused == "grid") {
-                let [a,b,c,d,e,f] = data.get("xform");
-                this.imSetState(data1.merge({xform: [a,b,c,d,
-                                                     (e+(x-x0)),
-                                                     (f+(y-y0))]}));
-            } else {
-                this.imSetState(data1.updateIn(["shapes", focused],
-                                               (shape) => translate(shape, x-x0, y-y0)));
-            }
-        }
-    }
+    let onMouseMove = useCallback((evt) => {
+        let [x,y] = [evt.clientX, evt.clientY];
+        setState((state) => {
+            // If dragging, move the focused shape
+            if (state.mouseDown) {
+                let [x0,y0] = state.mouseDown;
+                // Update the mouse position for future dragging
+                let state1 = {...state, mouseDown: [x,y]};
+                let focused = state1.focused;
+                if (focused == "grid") {
+                    let [a,b,c,d,e,f] = state1.xform;
+                    return {...state1, xform: [a,b,c,d, (e+(x-x0)), (f+(y-y0))]};
+                } else {
+                    let shape = state1.shapes[focused];
+                    return {...state1,
+                            shapes: {...state1.shapes,
+                                     [focused]: translate(shape, x-x0, y-y0)}};
+                }
+            } else {return state}
+        })
+    }, [])
 
-    onMouseUp(evt) {
-        let data = this.state.data;
-        this.imSetState(data.merge({mouseDown: null}));
-    }
+    let onMouseUp = useCallback((evt) => {
+        setState((state) => {return {...state, mouseDown: null}});
+    }, [])
 
-    // Adding a shape from the original model
-    addShape(model) {
-        let data = this.state.data;
-        let shapes = data.get("shapes");
-        let [a,b,c,d,tx,ty] = data.get("xform");
-        // Create an identity and associate it to a new shape model
-        let key = uuidv4();
-        // Randomize spawn location
-        let [rx,ry] = [(Math.random())*20, (Math.random())*20];
-        // set `key` so it'll be available on the Virtual-DOM, but set `id` so it goes to the real DOM
-        let shape = translate(model,-tx+rx,-ty+ry).merge({key:key, id:key});
-        this.imSetState(data.merge({shapes: shapes.set(key, shape)}));
-    }
+    let bindControl = useCallback((model) => {
+        // Let the app control everything
+        return {...model,
+                onMouseDown: onMouseDown,
+                onMouseMove: onMouseMove,
+                onMouseUp: onMouseUp};
+    }, [])
+    let rect = useMemo(() => bindControl(rectModel), []);
+    let circle = useMemo(() => bindControl(circleModel), []);
 
-    render() {
-        let data = this.state.data;
-        let shapes = data.get("shapes");
-        // Shapes that are actually rendered on the screen
-        let cpns = shapes.valueSeq().map(shapeToCpn).toJS();
+    // Adding a shape from a model
+    let addShape = useCallback((model) => {
+        setState((state) => {
+            let shapes = state.shapes;
+            let [a,b,c,d,tx,ty] = state.xform;
+            // Create an identity and associate it to a new shape model
+            let key = uuidv4();
+            // Randomize spawn location
+            let [rx,ry] = [(Math.random())*20, (Math.random())*20];
+            // set `key` so it'll be available on the Virtual-DOM, but set `id` so it goes to the real DOM
+            let shape = {...translate(model,-tx+rx+400,-ty+ry),
+                         key:key, id:key};
+            return {...state, shapes: {...shapes, [key]: shape}};
+        })
+    }, []);
 
-        return e(React.Fragment, {},
-                 // Controls
-                 e("div", {id:"controls"},
-                   e("button", {onClick: (evt) => {this.addShape(this.rect)}},
-                     "Rectangle"),
-                   e("button", {onClick: (evt) => {this.addShape(this.circle)}},
-                     "Circle")),
+    // Shapes that are actually rendered on the screen
+    let cpns = Object.values(state.shapes).map(shapeToCpn);
+    return e(React.Fragment, {},
+             // Controls
+             e("div", {id:"controls"},
+               e("button", {onClick: (evt) => addShape(rect)}, "Rectangle"),
+               e("button", {onClick: (evt) => addShape(circle)}, "Circle")),
 
-                 // The svg group, storing the whole digram
-                 e("svg", {id:"svg", width:801, height:481, xmlns:SVG_NS,},
-                   e("g", {id:"surface",
-                           x:-80, y:-80, width:961, heigth:641,},
-                     e("defs", {id:"defs"}, smallGrid, largeGrid),
-                     e("rect", {id:"grid", x:-80, y:-80, width:961, height: 641,
-                                transform: cssMatrix(data.get("xform")),
-                                fill: "url(#largeGrid)",
-                                // The grid is subject to zooming & panning
-                                onMouseUp: (evt) => this.onMouseUp(evt),
-                                onMouseMove: (evt) => this.onMouseMove(evt),
-                                onMouseDown: (evt) => this.onMouseDown(evt),
-                               })),
-                   // The shapes
-                   e("g", {id:"objects",
-                           transform: cssMatrix(data.get("xform"))},
-                     cpns)));
-    }
+             // The svg group, storing the whole digram
+             e("svg", {id:"svg", width:W, height:H, xmlns:SVG_NS,},
+               e("g", {id:"surface"},
+                 e("defs", {id:"defs"}, smallGrid, largeGrid),
+                 e("rect", bindControl({
+                     // The grid is subject to zooming & panning
+                     id:"grid", width:W, height: H,
+                     transform: cssMatrix(state.xform),
+                     fill: "url(#largeGrid)",
+                 }))),
+               // The shapes
+               e("g", {id:"objects",
+                       transform: cssMatrix(state.xform)},
+                 cpns))
+            );
 }
 
 let domContainer = document.getElementById("app");
