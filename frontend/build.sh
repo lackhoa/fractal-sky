ccdd0e8e1a1f6e8746cdffcaad7b601078ccfeb9
Khoa Vo <khoa.vo@hatch.live>
2021-09-17 08:48:07 +0300

    more diagrams

--- frontend/build.sh
+++ frontend/build.sh
@@ -7,8 +7,3 @@ sudo docker build -t lackhoa/fractal-sky:$VERSION .
 sudo docker run --rm -p 80:80 lackhoa/fractal-sky:$VERSION
 # Push to the docker registry
 sudo docker push lackhoa/fractal-sky:$VERSION
-
-# Deploying to Digital Ocean k8s (must change the version in "manifest.yaml" first)
-CLUSTER="fractal-sky"
-CONTEXT="do-lon1-${CLUSTER}"
-kubectl --context ${CONTEXT} apply -f manifest.yaml
